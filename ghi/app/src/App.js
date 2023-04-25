7a1771f3c7307d5382e38bc68edb1fa914e6746c
Brandon Dinh <dinhbrandon@gmail.com>
2023-04-24 18:38:33 -0700

    finish vehicle form

--- ghi/app/src/App.js
+++ ghi/app/src/App.js
@@ -1,6 +1,8 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import MainPage from './MainPage';
 import Nav from './Nav';
+import ListVehicleModels from './ListVehicleModelsBD';
+import VehicleModelForm from './VehicleModelFormBD';
 
 function App() {
   return (
@@ -9,6 +11,10 @@ function App() {
       <div className="container">
         <Routes>
           <Route path="/" element={<MainPage />} />
+          <Route path="models">
+            <Route path="" element={<ListVehicleModels />} />
+            <Route path="create" element={<VehicleModelForm />} />
+          </Route>
         </Routes>
       </div>
     </BrowserRouter>
---
+++ ghi/app/src/ListVehicleModelsBD.js
@@ -0,0 +1,9 @@
+import React, {useEffect, useState } from 'react';
+
+
+function ListVehicleModels(){
+    return(
+        <h1>Models</h1>
+    )
+}
+export default ListVehicleModels;
