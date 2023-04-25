527f8e5f6459d5180dc88a2b1eb7393958a1fb1d
Brandon Dinh <dinhbrandon@gmail.com>
2023-04-25 11:44:59 -0700

    working on poller

--- service/api/service_rest/models.py
+++ service/api/service_rest/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+
+class AutomobileVO(models.Model):
+    vin = models.CharField(max_length=200)
+
+
+class Technician(models.Model):
+    first_name = models.CharField(max_length=200)
+    last_name = models.CharField(max_length=200)
+    employee_id = models.PositiveSmallIntegerField()
+
+
+class Appointment(models.Model):
+    date_time = models.DateTimeField()
+    reason = models.TextField()
+    status = models.BooleanField()
+    # vin most likely will be JSON that we parse from automobile content
+    vin = models.CharField(max_length=200)
+    customer = models.CharField(max_length=200)
+    technician = models.ForeignKey(
+        Technician,
+        related_name="appointment",
+        on_delete=models.PROTECT
+    )
